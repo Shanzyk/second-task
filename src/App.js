010082f8c6d99e4c3bb5942c3bddbd54bce609ee
Admin <admin.push@hackerrank.workspace>
2022-11-14 17:50:44 +0000

    Auto save changes before Switch to Local IDE
    Commit by HackerRank

--- src/App.js
+++ src/App.js
@@ -1,24 +1,113 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [message, setMessage] = useState("");
+  const [error, setError] = useState(null);
+  const [isChecked, setIsChecked] = useState(false);
+
+  function isValidEmail(email) {
+    return /\S+@\S+\.\S+/.test(email);
+  }
+
+  function isInvalidValidEmail(email) {
+    return /\S+@\S+\.co/.test(email);
+  }
+
+  const handleChange = (event) => {
+    if (event.target.value.trim().length === 0) {
+      setError("Email address is required");
+    } else {
+      if (!isValidEmail(event.target.value)) {
+        setError("Please provide a valid e-mail address");
+      } else {
+        if (isInvalidValidEmail(event.target.value)) {
+          setError("We are not accepting subscribtions from Colombia emails");
+        } else {
+          setError(null);
+        }
+      }
+    }
+    setMessage(event.target.value);
+  };
+
+  const checkboxChange = (event) => {
+    setIsChecked(event.target.checked);
+    if (!isChecked) {
+      setError(null);
+    }
+  };
+
+  const buttonClick = (event) => {
+    if (!isChecked) {
+      setError("You must accept the terms and conditions");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+      <aside class="side">
+        <img class="logo" src="pineapple.png"></img>
+
+        <nav class="nav">
+          <a href="#">About</a>
+          <a href="#">How it works</a>
+          <a href="#">Contact</a>
+        </nav>
+        <div class="subs">
+          <h1>Subscribe to newsletter</h1>
+          <p>
+            Subscribe to our newsletter and get 10% discount on pineapple
+            glasses.
+          </p>
+        </div>
+        <form class="form">
+          <div class="emailpad">
+            <input
+              class="emailinput"
+              type="email"
+              name="email"
+              id="email-field"
+              placeholder="Type your email address here…"
+              value={message}
+              onChange={handleChange}
+              required
+            ></input>
+            <input
+              type="submit"
+              class="icon"
+              value=""
+              onClick={buttonClick}
+            ></input>
+            <span id="email-error" class="error">
+            {error}
+          </span>
+          </div>
+          <div class="checkbox">
+            <label>
+              <input
+                type="checkbox"
+                class="large"
+                name="checkbox"
+                id="checkbox"
+                onChange={checkboxChange}
+                required
+              />
+              <span>
+                I agree to <a href="#">terms of service</a>
+              </span>
+            </label>
+          </div>
+        </form>
+        <hr class="line"></hr>
+        <nav class="links">
+          <a href="#" class="face"></a>
+          <a href="#" class="inst"></a>
+          <a href="#" class="twit"></a>
+          <a href="#" class="yout"></a>
+        </nav>
+      </aside>
+      <img class="img" src="image.png"></img>
+    </body>
   );
 }
 
